5e5d392c908951dea9f5a4dc8ac96c83c5726447
Guy Marshall <guymarshall9@gmail.com>
2023-02-11 22:47:53 +0000

    Add collatz_max

--- app.py
+++ app.py
@@ -9,3 +9,16 @@ def collatz_steps(number):
         steps += 1
     
     return steps
+
+def collatz_max(number):
+    max_number = number
+
+    while number > 1:
+        if number % 2 == 0:
+            number = number / 2
+        else:
+            number = (3 * number) + 1
+        if number > max_number:
+            max_number = number
+    
+    return max_number
